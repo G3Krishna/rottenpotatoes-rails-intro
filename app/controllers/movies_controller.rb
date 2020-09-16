9283edd400d30dc4cdddb11540a1143f02234ec6
EC2 Default User <ec2-user@ip-172-31-4-113.us-east-2.compute.internal>
2020-09-16 04:16:19 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,7 +4,7 @@ class MoviesController < ApplicationController
     params.require(:movie).permit(:title, :rating, :description, :release_date)
   end
   
- def show
+  def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
@@ -12,27 +12,33 @@ class MoviesController < ApplicationController
 
   def index
     @movies = Movie.all
+    
+    #to store the name of column to be sorted
     column_to_be_sorted = params[:column_to_be_sorted]
     if(!column_to_be_sorted)
       column_to_be_sorted = session[:column_to_be_sorted]
     end
     
+    #to store the column that was sorted for the next operation
     session[:column_to_be_sorted]= column_to_be_sorted
+    #sort operation
     @movies = Movie.order(column_to_be_sorted)
+    #to store the available ratings
     @all_ratings = ['G','R','PG','PG-13']
+    #stores the ratings that was filtered by the user
     @filtered_rating = params[:ratings]
     if(!@filtered_rating)
       @filtered_rating=session[:ratings]
     end
-    
+    #stores the key values of the hash 'ratings' 
     if(@filtered_rating)
       @filtered_rating_keys = @filtered_rating.keys
     else
       @filtered_rating_keys = @all_ratings
     end
-    
+    #stores the ratings that were filtered by the user if needed for the next operation
     session[:ratings]=@filtered_rating
-    
+    #sorts the list of movies with given filtered ratings
     @movies = Movie.where(rating: @filtered_rating_keys).order(column_to_be_sorted)
 
     
